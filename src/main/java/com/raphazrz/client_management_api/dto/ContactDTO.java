bdabb61dd2c4c39ec8dfc1f7d9e9c4fdb7b6f645
RaphaZRZ <raphazlinked@gmail.com>
2026-06-19 00:34:32 -0300

    feat: add ContactType

--- src/main/java/com/raphazrz/client_management_api/dto/ContactDTO.java
+++ src/main/java/com/raphazrz/client_management_api/dto/ContactDTO.java
@@ -1,4 +1,6 @@
 package com.raphazrz.client_management_api.dto;
 
+import com.raphazrz.client_management_api.enumerator.ContactType;
+
 public record ContactDTO(ContactType contactType, String contact) {
 }
---
+++ src/main/java/com/raphazrz/client_management_api/enumerator/ContactType.java
@@ -0,0 +1,6 @@
+package com.raphazrz.client_management_api.enumerator;
+
+public enum ContactType {
+    PHONE,
+    EMAIL
+}
