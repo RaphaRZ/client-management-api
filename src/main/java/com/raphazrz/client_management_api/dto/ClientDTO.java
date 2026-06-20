a889c70ebb8041d898fa9240fcad07b8fc8d695a
RaphaZRZ <raphazlinked@gmail.com>
2026-06-20 16:28:12 -0300

    feat: add Client and Contact to DTOs

--- src/main/java/com/raphazrz/client_management_api/dto/ClientDTO.java
+++ src/main/java/com/raphazrz/client_management_api/dto/ClientDTO.java
@@ -1,4 +1,8 @@
 package com.raphazrz.client_management_api.dto;
 
-public record ClientDTO(String firstName, String lastName, String document, Contact contact) {
+import com.raphazrz.client_management_api.model.Contact;
+
+import java.util.List;
+
+public record ClientDTO(String firstName, String lastName, String document, List<Contact> contacts) {
 }
--- src/main/java/com/raphazrz/client_management_api/dto/ContactDTO.java
+++ src/main/java/com/raphazrz/client_management_api/dto/ContactDTO.java
@@ -1,6 +1,7 @@
 package com.raphazrz.client_management_api.dto;
 
 import com.raphazrz.client_management_api.enumerator.ContactType;
+import com.raphazrz.client_management_api.model.Client;
 
-public record ContactDTO(ContactType contactType, String contact) {
+public record ContactDTO(ContactType contactType, String contact, Client client) {
 }
