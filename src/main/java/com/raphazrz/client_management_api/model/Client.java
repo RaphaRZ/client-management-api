b49dcea5e07621bf39e8575f3b8cd94d2bbf8ac6
RaphaZRZ <raphazlinked@gmail.com>
2026-06-20 16:27:46 -0300

    feat: add Client

---
+++ src/main/java/com/raphazrz/client_management_api/model/Client.java
@@ -0,0 +1,27 @@
+package com.raphazrz.client_management_api.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+@Entity
+@Table(name = "client")
+public class Client {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String document;
+
+    private List<Contact> contacts;
+}
