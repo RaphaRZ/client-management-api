313aaed458e0d79506b64a81e626e4f9e37519ff
RaphaZRZ <raphazlinked@gmail.com>
2026-06-20 18:14:02 -0300

    feat: add createClient method

--- src/main/java/com/raphazrz/client_management_api/controller/ClientController.java
+++ src/main/java/com/raphazrz/client_management_api/controller/ClientController.java
@@ -1,5 +1,12 @@
 package com.raphazrz.client_management_api.controller;
 
+import com.raphazrz.client_management_api.dto.ClientDTO;
+import com.raphazrz.client_management_api.model.Client;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,5 +14,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/clients")
 public class ClientController {
+    @PostMapping
+    public ResponseEntity<Client> createClient(@Valid @RequestBody ClientDTO request) {
+        Client newClient = this.clientService.createClient(request);
 
+        return ResponseEntity.status(HttpStatus.CREATED).body(newClient);
+    }
 }
