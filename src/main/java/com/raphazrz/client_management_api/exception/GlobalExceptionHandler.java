6a87d7fad7be72a13f28bf10438fa71e3fcdeff1
RaphaZRZ <raphazlinked@gmail.com>
2026-06-28 19:20:22 -0300

    feat: add GlobalExceptionHandler

---
+++ src/main/java/com/raphazrz/client_management_api/exception/GlobalExceptionHandler.java
@@ -0,0 +1,15 @@
+package com.raphazrz.client_management_api.exception;
+
+import com.raphazrz.client_management_api.dto.other.ExceptionDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+}
