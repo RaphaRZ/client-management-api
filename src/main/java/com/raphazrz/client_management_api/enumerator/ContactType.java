021b7be0c6eeb0ca1c6218b0c13663f3efd38149
RaphaZRZ <raphazlinked@gmail.com>
2026-06-28 00:23:50 -0300

    feat: add fromType method

--- src/main/java/com/raphazrz/client_management_api/enumerator/ContactType.java
+++ src/main/java/com/raphazrz/client_management_api/enumerator/ContactType.java
@@ -1,6 +1,25 @@
 package com.raphazrz.client_management_api.enumerator;
 
 public enum ContactType {
-    PHONE,
-    EMAIL
+    PHONE(1),
+    EMAIL(2);
+
+    private final int type;
+
+    ContactType(int type) {
+        this.type = type;
+    }
+
+    public static ContactType fromType(int type) {
+        for (ContactType contact : ContactType.values()) {
+            if (contact.getType() == type)
+                return contact;
+        }
+
+        throw new InvalidContactTypeException("Invalid contact type. Valid values are: 1 (PHONE) and 2 (EMAIL).");
+    }
+
+    public int getType() {
+        return type;
+    }
 }
