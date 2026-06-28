10718274880b4e54d801b1d8b26b826a6bd783e2
RaphaZRZ <raphazlinked@gmail.com>
2026-06-28 00:34:28 -0300

    feat: add RegexConstants

---
+++ src/main/java/com/raphazrz/client_management_api/constant/RegexConstants.java
@@ -0,0 +1,6 @@
+package com.raphazrz.client_management_api.constant;
+
+public class RegexConstants {
+    public static final String DOCUMENT_REGEX = "[0-9]{11}";
+    public static final String FORMATTED_DOCUMENT_REGEX = "[0-9]{3}[.][0-9]{3}[.][0-9]{3}[-][0-9]{2}";
+}
