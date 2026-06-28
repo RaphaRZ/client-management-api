3e2cfd0dc2226c52a8ab6cf1fe2087fd99a4a397
RaphaZRZ <raphazlinked@gmail.com>
2026-06-28 18:47:48 -0300

    feat: add jakarta validations

--- src/main/java/com/raphazrz/client_management_api/dto/request/ClientRequestDTO.java
+++ src/main/java/com/raphazrz/client_management_api/dto/request/ClientRequestDTO.java
@@ -1,8 +1,25 @@
 package com.raphazrz.client_management_api.dto.request;
 
+import com.raphazrz.client_management_api.constant.RegexConstants;
 import com.raphazrz.client_management_api.model.Contact;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 
 import java.util.List;
 
-public record ClientRequestDTO(String firstName, String lastName, String document, List<Contact> contacts) {
+public record ClientRequestDTO(
+        @NotBlank(message = "First name is required.")
+        String firstName,
+
+        @NotBlank(message = "Last name is required.")
+        String lastName,
+
+        @NotBlank(message = "Document is required.")
+        @Pattern(
+                regexp = RegexConstants.DOCUMENT_REGEX + "|" + RegexConstants.FORMATTED_DOCUMENT_REGEX,
+                message = "Document must contain 11 digits or follow the format XXX.XXX.XXX-XX."
+        )
+        String document,
+
+        List<Contact> contacts) {
 }
