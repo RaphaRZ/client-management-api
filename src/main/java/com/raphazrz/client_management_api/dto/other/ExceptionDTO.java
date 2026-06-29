eeacd6b1c7497e29f3a4fd5eddd7e71edd530d87
RaphaZRZ <raphazlinked@gmail.com>
2026-06-28 23:09:58 -0300

    refactor: change ExceptionDTO type from class to record

--- src/main/java/com/raphazrz/client_management_api/dto/other/ExceptionDTO.java
+++ src/main/java/com/raphazrz/client_management_api/dto/other/ExceptionDTO.java
@@ -1,15 +1,7 @@
 package com.raphazrz.client_management_api.dto.other;
 
-import lombok.Getter;
 
-
-@Getter
-public class ExceptionDTO {
-    private final String message;
-    private final int statusCode;
-
-    public ExceptionDTO(String message, int statusCode) {
-        this.message = message;
-        this.statusCode = statusCode;
-    }
+public record ExceptionDTO(
+        String message,
+        int statusCode) {
 }
