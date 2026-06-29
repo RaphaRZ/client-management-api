533fff8fc8cba1dff7f92d78bdf17c1e33fba1f5
RaphaZRZ <raphazlinked@gmail.com>
2026-06-29 00:41:37 -0300

    feat: add validationErrors attribute and constructor

--- src/main/java/com/raphazrz/client_management_api/dto/other/ExceptionDTO.java
+++ src/main/java/com/raphazrz/client_management_api/dto/other/ExceptionDTO.java
@@ -1,7 +1,18 @@
 package com.raphazrz.client_management_api.dto.other;
 
 
+import java.util.Map;
+
 public record ExceptionDTO(
         String message,
+        Map<String, String> validationErrors,
         int statusCode) {
+
+    public ExceptionDTO(String message, int statusCode) {
+        this(message, null, statusCode);
+    }
+
+    public ExceptionDTO(Map<String, String> validationErrors, int statusCode) {
+        this("Validation failed.", validationErrors, statusCode);
+    }
 }
