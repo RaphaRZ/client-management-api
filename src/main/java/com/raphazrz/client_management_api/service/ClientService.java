58d01fe60a27c9ea48810f6255a8d00c1f430f94
RaphaZRZ <raphazlinked@gmail.com>
2026-07-02 01:11:50 -0300

    feat: add validateUniqueDocument method

--- src/main/java/com/raphazrz/client_management_api/service/ClientService.java
+++ src/main/java/com/raphazrz/client_management_api/service/ClientService.java
@@ -2,6 +2,7 @@ package com.raphazrz.client_management_api.service;
 
 import com.raphazrz.client_management_api.dto.request.ClientRequestDTO;
 import com.raphazrz.client_management_api.dto.response.ClientResponseDTO;
+import com.raphazrz.client_management_api.exception.DuplicateDocumentException;
 import com.raphazrz.client_management_api.mapper.ClientMapper;
 import com.raphazrz.client_management_api.model.Client;
 import com.raphazrz.client_management_api.repository.ClientRepository;
@@ -19,10 +20,16 @@ public class ClientService {
     }
 
     public ClientResponseDTO createClient(ClientRequestDTO clientRequestDTO) {
-        Client newClient = ClientMapper.toEntity(clientRequestDTO);
+        validateUniqueDocument(clientRequestDTO.document());
 
+        Client newClient = ClientMapper.toEntity(clientRequestDTO);
         Client savedClient = saveClient(newClient);
 
         return ClientMapper.toResponseDTO(savedClient);
+    }
+
+    public void validateUniqueDocument(String document) {
+        if (clientRepository.existsByDocument(document))
+            throw new DuplicateDocumentException("Document already registered.");
     }
 }
