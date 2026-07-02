77202379c88d5515283feeda881ecd41f4cbb90d
RaphaZRZ <raphazlinked@gmail.com>
2026-07-02 01:47:42 -0300

    feat: add validateClientExistence method

--- src/main/java/com/raphazrz/client_management_api/service/ContactService.java
+++ src/main/java/com/raphazrz/client_management_api/service/ContactService.java
@@ -1,4 +1,29 @@
 package com.raphazrz.client_management_api.service;
 
+import com.raphazrz.client_management_api.dto.request.ContactRequestDTO;
+import com.raphazrz.client_management_api.dto.response.ClientResponseDTO;
+import com.raphazrz.client_management_api.dto.response.ContactResponseDTO;
+import com.raphazrz.client_management_api.exception.ClientNotFoundException;
+import com.raphazrz.client_management_api.repository.ClientRepository;
+import com.raphazrz.client_management_api.repository.ContactRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+
+@RequiredArgsConstructor
+@Service
 public class ContactService {
+    private final ClientRepository clientRepository;
+    private final ContactRepository contactRepository;
+
+    public ContactResponseDTO createContact(ContactRequestDTO contactRequestDTO) {
+        validateClientExistence(contactRequestDTO.clientId());
+
+
+    }
+
+    private void validateClientExistence(Long clientId) {
+        if (!clientRepository.existsById(clientId))
+            throw new ClientNotFoundException("Client not found.");
+    }
 }
