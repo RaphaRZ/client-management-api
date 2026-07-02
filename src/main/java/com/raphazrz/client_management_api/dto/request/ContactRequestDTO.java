578c3895e46851a2567b02ca1f432c4e053abcce
RaphaZRZ <raphazlinked@gmail.com>
2026-07-02 02:31:22 -0300

    refactor: change contactType attribute from ContactType to Integer

--- src/main/java/com/raphazrz/client_management_api/dto/request/ContactRequestDTO.java
+++ src/main/java/com/raphazrz/client_management_api/dto/request/ContactRequestDTO.java
@@ -1,12 +1,11 @@
 package com.raphazrz.client_management_api.dto.request;
 
-import com.raphazrz.client_management_api.enumerator.ContactType;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
 public record ContactRequestDTO(
         @NotNull(message = "Contact type is required.")
-        ContactType contactType,
+        Integer contactType,
 
         @NotBlank(message = "Contact is required.")
         String contact,
