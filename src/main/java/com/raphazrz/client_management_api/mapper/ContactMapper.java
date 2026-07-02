ce6aa5f7e291ddb0d52f1c557eaea1e2e85af8b2
RaphaZRZ <raphazlinked@gmail.com>
2026-07-02 02:19:28 -0300

    feat: add toResponseDTO method

--- src/main/java/com/raphazrz/client_management_api/mapper/ContactMapper.java
+++ src/main/java/com/raphazrz/client_management_api/mapper/ContactMapper.java
@@ -1,11 +1,19 @@
 package com.raphazrz.client_management_api.mapper;
 
 import com.raphazrz.client_management_api.dto.request.ContactRequestDTO;
-import com.raphazrz.client_management_api.enumerator.ContactType;
+import com.raphazrz.client_management_api.dto.response.ContactResponseDTO;
 import com.raphazrz.client_management_api.model.Client;
 import com.raphazrz.client_management_api.model.Contact;
 
 public class ContactMapper {
+    public static ContactResponseDTO toResponseDTO(Contact contact) {
+        return new ContactResponseDTO(
+                contact.getContactType(),
+                contact.getContact(),
+                contact.getClientId()
+        );
+    }
+
     public static Contact toEntity(ContactRequestDTO contactRequestDTO) {
         return new Contact(
                 contactRequestDTO.contactType(),
