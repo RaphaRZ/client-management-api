5af7ad453e3462f6e137c1f0c760f4a08d63c330
RaphaZRZ <raphazlinked@gmail.com>
2026-07-02 02:24:11 -0300

    feat: add createContact method

--- src/main/java/com/raphazrz/client_management_api/controller/ContactController.java
+++ src/main/java/com/raphazrz/client_management_api/controller/ContactController.java
@@ -1,10 +1,28 @@
 package com.raphazrz.client_management_api.controller;
 
+import com.raphazrz.client_management_api.dto.request.ContactRequestDTO;
+import com.raphazrz.client_management_api.dto.response.ContactResponseDTO;
+import com.raphazrz.client_management_api.service.ContactService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/contacts")
 public class ContactController {
+    private final ContactService contactService;
+
+    @PostMapping
+    public ResponseEntity<ContactResponseDTO> createContact(@Valid @RequestBody ContactRequestDTO request) {
+        ContactResponseDTO newContact = contactService.createContact(request);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(newContact);
+    }
 }
