61ae720025f9cd34637962ee0e97fd64c34e595e
RaphaZRZ <raphazlinked@gmail.com>
2026-07-03 23:50:15 -0300

    feat: add CdeleteContact method

--- src/main/java/com/raphazrz/client_management_api/service/ContactService.java
+++ src/main/java/com/raphazrz/client_management_api/service/ContactService.java
@@ -3,6 +3,7 @@ package com.raphazrz.client_management_api.service;
 import com.raphazrz.client_management_api.dto.request.ContactRequestDTO;
 import com.raphazrz.client_management_api.dto.response.ContactResponseDTO;
 import com.raphazrz.client_management_api.exception.ClientNotFoundException;
+import com.raphazrz.client_management_api.exception.ContactNotFoundException;
 import com.raphazrz.client_management_api.mapper.ContactMapper;
 import com.raphazrz.client_management_api.model.Client;
 import com.raphazrz.client_management_api.model.Contact;
@@ -10,6 +11,7 @@ import com.raphazrz.client_management_api.repository.ClientRepository;
 import com.raphazrz.client_management_api.repository.ContactRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 
 @RequiredArgsConstructor
@@ -18,6 +20,7 @@ public class ContactService {
     private final ClientRepository clientRepository;
     private final ContactRepository contactRepository;
 
+    @Transactional
     public ContactResponseDTO createContact(ContactRequestDTO contactRequestDTO) {
         Client client = findClientById(contactRequestDTO.clientId());
 
@@ -27,6 +30,14 @@ public class ContactService {
         Contact savedContact = saveContact(newContact);
 
         return ContactMapper.toResponseDTO(savedContact);
+    }
+
+    @Transactional
+    public void deleteContact(Long contactId) {
+        Contact contact = contactRepository.findById(contactId)
+                .orElseThrow(() -> new ContactNotFoundException("Contact not found."));
+
+        contactRepository.delete(contact);
     }
 
     private Client findClientById(Long clientId) {
