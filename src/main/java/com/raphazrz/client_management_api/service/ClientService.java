4a2a1d53bf29ec8a178d15e145ff73a8786d85ee
RaphaZRZ <raphazlinked@gmail.com>
2026-07-04 00:32:52 -0300

    feat: add getClientById method

--- src/main/java/com/raphazrz/client_management_api/service/ClientService.java
+++ src/main/java/com/raphazrz/client_management_api/service/ClientService.java
@@ -2,12 +2,14 @@ package com.raphazrz.client_management_api.service;
 
 import com.raphazrz.client_management_api.dto.request.ClientRequestDTO;
 import com.raphazrz.client_management_api.dto.response.ClientResponseDTO;
+import com.raphazrz.client_management_api.exception.ClientNotFoundException;
 import com.raphazrz.client_management_api.exception.DuplicateDocumentException;
 import com.raphazrz.client_management_api.mapper.ClientMapper;
 import com.raphazrz.client_management_api.model.Client;
 import com.raphazrz.client_management_api.repository.ClientRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 
 @RequiredArgsConstructor
@@ -15,6 +17,7 @@ import org.springframework.stereotype.Service;
 public class ClientService {
     private final ClientRepository clientRepository;
 
+    @Transactional
     public ClientResponseDTO createClient(ClientRequestDTO clientRequestDTO) {
         validateUniqueDocument(clientRequestDTO.document());
 
@@ -22,6 +25,16 @@ public class ClientService {
         Client savedClient = saveClient(newClient);
 
         return ClientMapper.toResponseDTO(savedClient);
+    }
+
+    @Transactional(readOnly = true)
+    public ClientResponseDTO getClientById(Long id) {
+        return ClientMapper.toResponseDTO(findClientById(id));
+    }
+
+    private Client findClientById(Long id) {
+        return clientRepository.findById(id)
+                .orElseThrow(() -> new ClientNotFoundException("Client not found."));
     }
 
     public void validateUniqueDocument(String document) {
