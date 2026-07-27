93e24f75005460f3889c88b94bec3b3cfd3c6592
RaphaZRZ <raphazlinked@gmail.com>
2026-07-27 03:18:25 -0300

    test: add ClientQueryServiceTest.java

---
+++ src/test/java/com/raphazrz/client_management_api/service/ClientQueryServiceTest.java
@@ -0,0 +1,15 @@
+package com.raphazrz.client_management_api.service;
+
+
+import com.raphazrz.client_management_api.repository.ClientRepository;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class ClientQueryServiceTest {
+    @Mock
+    private ClientRepository clientRepository;
+
+
+}
