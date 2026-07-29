34bf0780fe33d47154679b23c325051e17af74b6
RaphaZRZ <raphazlinked@gmail.com>
2026-07-29 05:26:54 -0300

    test: add createContactRequestDTO helper

--- src/test/java/com/raphazrz/client_management_api/util/TestDataFactory.java
+++ src/test/java/com/raphazrz/client_management_api/util/TestDataFactory.java
@@ -1,6 +1,7 @@
 package com.raphazrz.client_management_api.util;
 
 import com.raphazrz.client_management_api.dto.request.ClientRequestDTO;
+import com.raphazrz.client_management_api.dto.request.ContactRequestDTO;
 import com.raphazrz.client_management_api.dto.request.UpdateClientRequestDTO;
 import com.raphazrz.client_management_api.dto.response.ClientContactResponseDTO;
 import com.raphazrz.client_management_api.enumerator.ContactType;
@@ -43,6 +44,14 @@ public class TestDataFactory {
                 faker.name().firstName(),
                 faker.name().lastName(),
                 faker.number().digits(11)
+        );
+    }
+
+    public static ContactRequestDTO createContactRequestDTO() {
+        return new ContactRequestDTO(
+                faker.number().numberBetween(1, 3),
+                faker.text().text(),
+                faker.number().randomNumber()
         );
     }
 }
