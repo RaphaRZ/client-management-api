ee45ac85daad7553619789e7aef8ec79ff30ff0e
RaphaZRZ <raphazlinked@gmail.com>
2026-07-29 04:43:10 -0300

    Revert "test: add createClientResponseDTO method"
    This reverts commit 0096e5ca9cfcc0b0741db465422409702be218ab.

--- src/test/java/com/raphazrz/client_management_api/service/ClientServiceTest.java
+++ src/test/java/com/raphazrz/client_management_api/service/ClientServiceTest.java
@@ -195,12 +195,4 @@ public class ClientServiceTest {
                 faker.number().digits(11)
         );
     }
-
-    private ClientResponseDTO createClientResponseDTO() {
-        return new ClientResponseDTO(
-                faker.name().firstName(),
-                faker.name().lastName(),
-                faker.number().digits(11),
-                new ArrayList<>()
-        );
 }
