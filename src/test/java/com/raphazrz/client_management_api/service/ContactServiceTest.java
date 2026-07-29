7ebdd6814b96947527a49e494178f21ef78127a1
RaphaZRZ <raphazlinked@gmail.com>
2026-07-29 06:23:10 -0300

    test: add updateContactByIdSuccess test

--- src/test/java/com/raphazrz/client_management_api/service/ContactServiceTest.java
+++ src/test/java/com/raphazrz/client_management_api/service/ContactServiceTest.java
@@ -1,12 +1,13 @@
 package com.raphazrz.client_management_api.service;
 
 import com.raphazrz.client_management_api.dto.request.ContactRequestDTO;
+import com.raphazrz.client_management_api.dto.request.UpdateContactRequestDTO;
 import com.raphazrz.client_management_api.dto.response.ContactResponseDTO;
+import com.raphazrz.client_management_api.enumerator.ContactType;
 import com.raphazrz.client_management_api.mapper.ContactMapper;
 import com.raphazrz.client_management_api.model.Client;
 import com.raphazrz.client_management_api.model.Contact;
 import com.raphazrz.client_management_api.repository.ContactRepository;
-import com.raphazrz.client_management_api.util.TestDataFactory;
 import net.datafaker.Faker;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -15,8 +16,12 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.util.ArrayList;
+import java.util.Optional;
 
+import static com.raphazrz.client_management_api.util.TestDataFactory.createClient;
+import static com.raphazrz.client_management_api.util.TestDataFactory.createContactRequestDTO;
+import static com.raphazrz.client_management_api.util.TestDataFactory.createUpdateContactRequestDTO;
+import static com.raphazrz.client_management_api.util.TestDataFactory.createContact;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
@@ -41,8 +46,8 @@ public class ContactServiceTest {
     @DisplayName("Should return a new contact as ContactResponseDTO successfully.")
     void createContactSuccess() {
         // Arrange
-        ContactRequestDTO request = TestDataFactory.createContactRequestDTO();
-        Client client = TestDataFactory.createClient();
+        ContactRequestDTO request = createContactRequestDTO();
+        Client client = createClient();
 
         Contact savedContact = ContactMapper.toEntity(request);
         savedContact.setClient(client);
@@ -59,5 +64,24 @@ public class ContactServiceTest {
         assertEquals(expectedResponse, result);
         verify(clientQueryService).findClientById(request.clientId());
         verify(contactRepository).save(any(Contact.class));
+    }
+
+    @Test
+    @DisplayName("Should update the contact successfully.")
+    void updateContactByIdSuccess() {
+        // Arrange
+        UpdateContactRequestDTO request = createUpdateContactRequestDTO();
+        Contact updatedContact = createContact();
+
+        when(contactRepository.findById(1L)).thenReturn(Optional.of(updatedContact));
+
+        // Act
+        ContactResponseDTO result = contactService.updateContactById(1L, request);
+
+        // Assert
+        assertEquals(ContactType.fromType(request.contactType()), updatedContact.getContactType());
+        assertEquals(request.contact(), updatedContact.getContact());
+        assertEquals(ContactMapper.toResponseDTO(updatedContact), result);
+        verify(contactRepository).findById(1L);
     }
 }
