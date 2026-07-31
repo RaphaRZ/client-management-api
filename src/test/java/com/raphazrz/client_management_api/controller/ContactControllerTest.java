bb87e5b213ea21ce975babe879a69da67f6fa4af
RaphaZRZ <raphazlinked@gmail.com>
2026-07-31 04:29:44 -0300

    test: add createContactCreated test

--- src/test/java/com/raphazrz/client_management_api/controller/ContactControllerTest.java
+++ src/test/java/com/raphazrz/client_management_api/controller/ContactControllerTest.java
@@ -1,13 +1,24 @@
 package com.raphazrz.client_management_api.controller;
 
+import com.raphazrz.client_management_api.dto.request.ContactRequestDTO;
+import com.raphazrz.client_management_api.dto.response.ContactResponseDTO;
 import com.raphazrz.client_management_api.service.ContactService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import tools.jackson.databind.ObjectMapper;
+
+import static com.raphazrz.client_management_api.util.TestDataFactory.createContactRequestDTO;
+import static com.raphazrz.client_management_api.util.TestDataFactory.createContactResponseDTO;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
 @WebMvcTest(ContactController.class)
@@ -23,5 +34,23 @@ public class ContactControllerTest {
 
 
     @Test
-    @DisplayName("")
+    @DisplayName("Should return status 201 Created.")
+    void createContactCreated() throws Exception {
+        // Arrange
+        ContactRequestDTO request = createContactRequestDTO();
+        ContactResponseDTO expectedResponse = createContactResponseDTO();
+
+        when(contactService.createContact(request)).thenReturn(expectedResponse);
+
+        // Act & Assert
+        mockMvc.perform(post("/contacts")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request)))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.contactType").value(expectedResponse.contactType().name()))
+                .andExpect(jsonPath("$.contact").value(expectedResponse.contact()))
+                .andExpect(jsonPath("$.clientId").value(expectedResponse.clientId()));
+
+        verify(contactService).createContact(request);
+    }
 }
