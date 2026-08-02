01066b92ebc2a6870ab3733e4cbec5d2bdb71499
RaphaZRZ <raphazlinked@gmail.com>
2026-08-02 20:28:58 -0300

    feat: add ClientControllerDocumentation interface

---
+++ src/main/java/com/raphazrz/client_management_api/controller/docs/ClientControllerDocumentation.java
@@ -0,0 +1,15 @@
+package com.raphazrz.client_management_api.controller.docs;
+
+import com.raphazrz.client_management_api.dto.request.ClientRequestDTO;
+import com.raphazrz.client_management_api.dto.response.ClientResponseDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+
+public interface ClientControllerDocumentation {
+
+
+}
