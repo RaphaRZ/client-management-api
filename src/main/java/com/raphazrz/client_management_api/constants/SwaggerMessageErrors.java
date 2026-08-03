5834cbfecd75ec1add2d9968df8f51c10c241daa
RaphaZRZ <raphazlinked@gmail.com>
2026-08-03 02:57:50 -0300

    feat: add INVALID_REQUEST_DATA constants

--- src/main/java/com/raphazrz/client_management_api/constants/SwaggerMessageErrors.java
+++ src/main/java/com/raphazrz/client_management_api/constants/SwaggerMessageErrors.java
@@ -1,23 +1,34 @@
 package com.raphazrz.client_management_api.constants;
 
 public final class SwaggerMessageErrors {
+    public static final String INVALID_REQUEST_DATA_DESCRIPTION = "Invalid request data.";
+    public static final String INVALID_REQUEST_DATA_VALUE = """
+            {
+              "message": "Validation failed.",
+              "validationErrors": {
+                "document": "Document is required."
+              },
+              "statusCode": 400
+            }
+            """;
+
     public static final String INVALID_PARAMETER_ID_DESCRIPTION = "Invalid value for parameter 'id'.";
     public static final String INVALID_PARAMETER_ID_VALUE = """
-    {
-      "message": "Invalid value for parameter 'id'.",
-      "validationErrors": {},
-      "statusCode": 400
-    }
-    """;
+            {
+              "message": "Invalid value for parameter 'id'.",
+              "validationErrors": {},
+              "statusCode": 400
+            }
+            """;
 
     public static final String CLIENT_NOT_FOUND_DESCRIPTION = "Client not found.";
     public static final String CLIENT_NOT_FOUND_VALUE = """
-    {
-      "message": "Client not found.",
-      "validationErrors": {},
-      "statusCode": 404
-    }
-    """;
+            {
+              "message": "Client not found.",
+              "validationErrors": {},
+              "statusCode": 404
+            }
+            """;
 
     private SwaggerMessageErrors() {
         throw new UnsupportedOperationException("Utility class");
