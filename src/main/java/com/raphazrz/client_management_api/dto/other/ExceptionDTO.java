3c49d349f4c3fe4f2f36b8b12e3208f831e025c9
RaphaZRZ <raphazlinked@gmail.com>
2026-08-02 22:40:56 -0300

    feat: add @Schema annotations

--- src/main/java/com/raphazrz/client_management_api/dto/other/ExceptionDTO.java
+++ src/main/java/com/raphazrz/client_management_api/dto/other/ExceptionDTO.java
@@ -1,13 +1,19 @@
 package com.raphazrz.client_management_api.dto.other;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 
 import java.util.HashMap;
 import java.util.Map;
 
 
 public record ExceptionDTO(
+        @Schema(description = "Description of the error.")
         String message,
+
+        @Schema(description = "Validation errors by field.")
         Map<String, String> validationErrors,
+
+        @Schema(example = "404")
         int statusCode) {
 
     public ExceptionDTO(String message, int statusCode) {
