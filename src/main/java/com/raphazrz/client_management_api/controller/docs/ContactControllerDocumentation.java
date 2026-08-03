6780f5667b6287bb8c0f52543aa44e63dd274d92
RaphaZRZ <raphazlinked@gmail.com>
2026-08-02 21:42:38 -0300

    feat: add updateContact abstract method

--- src/main/java/com/raphazrz/client_management_api/controller/docs/ContactControllerDocumentation.java
+++ src/main/java/com/raphazrz/client_management_api/controller/docs/ContactControllerDocumentation.java
@@ -1,6 +1,7 @@
 package com.raphazrz.client_management_api.controller.docs;
 
 import com.raphazrz.client_management_api.dto.request.ContactRequestDTO;
+import com.raphazrz.client_management_api.dto.request.UpdateContactRequestDTO;
 import com.raphazrz.client_management_api.dto.response.ContactResponseDTO;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
@@ -25,4 +26,21 @@ public interface ContactControllerDocumentation {
             )
     })
     ResponseEntity<ContactResponseDTO> createContact(ContactRequestDTO request);
+
+    @Operation(summary = "Update contact by ID.")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Contact updated successfully."
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "Invalid request data."
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Contact not found."
+            )
+    })
+    ResponseEntity<ContactResponseDTO> updateContact(Long id, UpdateContactRequestDTO request);
 }
