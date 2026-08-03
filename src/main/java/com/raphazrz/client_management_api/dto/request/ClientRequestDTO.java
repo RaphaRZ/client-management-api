b1a4f0746175ed83b3bfd3bc2376b6fb51bfcbb9
RaphaZRZ <raphazlinked@gmail.com>
2026-08-02 21:57:49 -0300

    feat: add @Schema annotations

--- src/main/java/com/raphazrz/client_management_api/dto/request/ClientRequestDTO.java
+++ src/main/java/com/raphazrz/client_management_api/dto/request/ClientRequestDTO.java
@@ -1,17 +1,21 @@
 package com.raphazrz.client_management_api.dto.request;
 
 import com.raphazrz.client_management_api.constant.RegexConstants;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 
 
 public record ClientRequestDTO(
+        @Schema(example = "Albert")
         @NotBlank(message = "First name is required.")
         String firstName,
 
+        @Schema(example = "Wesker")
         @NotBlank(message = "Last name is required.")
         String lastName,
 
+        @Schema(example = "00123456789")
         @NotBlank(message = "Document is required.")
         @Pattern(
                 regexp = RegexConstants.DOCUMENT_REGEX + "|" + RegexConstants.FORMATTED_DOCUMENT_REGEX,
