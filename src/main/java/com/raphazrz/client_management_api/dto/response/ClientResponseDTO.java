bf15e4d83379768d7ca84002c80b0ec6ce7a4bbf
RaphaZRZ <raphazlinked@gmail.com>
2026-08-02 22:14:51 -0300

    feat: add @Schema annotations

--- src/main/java/com/raphazrz/client_management_api/dto/response/ClientResponseDTO.java
+++ src/main/java/com/raphazrz/client_management_api/dto/response/ClientResponseDTO.java
@@ -1,6 +1,19 @@
 package com.raphazrz.client_management_api.dto.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 import java.util.List;
 
-public record ClientResponseDTO(String firstName, String lastName, String document, List<ClientContactResponseDTO> contacts) {
+
+public record ClientResponseDTO(
+        @Schema(example = "Albert")
+        String firstName,
+
+        @Schema(example = "Wesker")
+        String lastName,
+
+        @Schema(example = "00123456789")
+        String document,
+
+        List<ClientContactResponseDTO> contacts) {
 }
