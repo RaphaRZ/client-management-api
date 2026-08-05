5359f6995db562cbe9c5e3fa83ffdc49fb7faf71
RaphaZRZ <raphazlinked@gmail.com>
2026-08-05 09:01:12 -0300

    feat(api): add findByDocument method

--- src/main/java/com/raphazrz/client_management_api/repository/ClientRepository.java
+++ src/main/java/com/raphazrz/client_management_api/repository/ClientRepository.java
@@ -4,8 +4,13 @@ import com.raphazrz.client_management_api.model.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
+
 @Repository
 public interface ClientRepository extends JpaRepository<Client, Long> {
+    Optional<Client> findByDocument(String document);
+
     boolean existsByDocument(String document);
     boolean existsByDocumentAndIdNot(String document, Long id);
 }
