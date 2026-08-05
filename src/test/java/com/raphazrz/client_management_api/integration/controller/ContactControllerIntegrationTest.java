4821c8b2e53b8a5b4c0c45f39561b80fd13a0154
RaphaZRZ <raphazlinked@gmail.com>
2026-08-05 03:34:13 -0300

    test: add ContactControllerIntegrationTest.class

---
+++ src/test/java/com/raphazrz/client_management_api/integration/controller/ContactControllerIntegrationTest.java
@@ -0,0 +1,24 @@
+package com.raphazrz.client_management_api.integration.controller;
+
+import com.raphazrz.client_management_api.integration.base.BaseIntegrationTest;
+import com.raphazrz.client_management_api.repository.ClientRepository;
+import com.raphazrz.client_management_api.repository.ContactRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+
+
+public class ContactControllerIntegrationTest extends BaseIntegrationTest {
+    private static final String BASE_CONTACTS_URL = "/contacts";
+    private static final String BASE_CLIENTS_URL = "/clients";
+
+    @Autowired
+    ContactRepository contactRepository;
+
+    @Autowired
+    ClientRepository clientRepository;
+
+    @BeforeEach
+    void setUp() {
+        clientRepository.deleteAll();
+    }
+}
