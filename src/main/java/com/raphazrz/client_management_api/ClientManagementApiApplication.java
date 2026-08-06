ef4a7b287d4f609c05329e86f2cb0c811a9e4c4b
RaphaZRZ <raphazlinked@gmail.com>
2026-08-06 08:29:55 -0300

    refactor: remove public redundant modifier

--- src/main/java/com/raphazrz/client_management_api/ClientManagementApiApplication.java
+++ src/main/java/com/raphazrz/client_management_api/ClientManagementApiApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ClientManagementApiApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(ClientManagementApiApplication.class, args);
-	}
-
+    static void main(String[] args) {
+        SpringApplication.run(ClientManagementApiApplication.class, args);
+    }
 }
